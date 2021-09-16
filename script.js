c47d24bd04fcffec95865dd7936b9e6988a2a8bc
Shahzaib Arshad <shahzaibars.work@gmail.com>
2021-09-16 10:48:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("file running")
+
+
+
+// function changeText(){
+//     // in this way whole innerHTML will be changed straight way
+//     // document.getElementById("heading").innerHTML="heading added";
+    
+//     // in this way innerHTML will be intended 
+//     document.getElementById("heading").innerHTML+=" heading added";
+// }
+
+
+// simple example of arrow function.
+changeText = () => document.getElementById("heading").write(" heading added through arrow function")
+
